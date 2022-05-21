958903c9525eae5de9ad448072338165aa71b559
naveeng143 <45759489+naveeng143@users.noreply.github.com>
2022-05-21 17:44:18 +0530

    Add files via upload
    Selenium Demo

---
+++ java/src/test/java/dev/selenium/getting_started/FirstScriptTest.java
@@ -0,0 +1,37 @@
+package dev.selenium.getting_started;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class FirstScriptTest {
+	public WebDriver driver;
+
+	@Test
+	public void eightComponents() {
+		driver = new ChromeDriver();
+
+		driver.get("https://www.facebook.com/");
+
+		Assertions.assertEquals("Facebook – log in or sign up", driver.getTitle());
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+		WebElement searchBox = driver.findElement(By.id("email"));
+//		WebElement searchButton = driver.findElement(By.name("passs"));
+
+		searchBox.sendKeys("Selenium4");
+
+		WebElement firstResult = new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(By.id("nav")));
+		firstResult.sendKeys("Selenium");
+
+		driver.quit();
+	}
+}
